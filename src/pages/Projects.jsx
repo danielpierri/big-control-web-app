8722c88e7ab8be6cb29ca2106b421d44e09ab0d8
Daniel Pierri <cyberpierri@gmail.com>
2023-06-13 14:41:26 -0300

    Adding content

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,69 +12,55 @@ export default function Project() {
       <div className="projects">
         <ScrollToTopBtn />
         <div className="project__container">
-          <h2 className="project__title">PROJETOS DE CONDOMÍNIO</h2>
+          <h2 className="project__title">PROJETOS PARA CONDOMÍNIOS E EMPRESAS</h2>
           <div className="project__img__container">
             <img className="project__img" src={projetosHome} alt="" />
           </div>
           <p className="project__text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            dicta animi voluptatibus voluptate. Delectus neque libero quis
-            laborum vitae deleniti deserunt facilis perspiciatis modi, earum,
-            praesentium tempora distinctio sunt accusamus recusandae totam nemo
-            eum natus sapiente! Asperiores ullam modi officia ducimus incidunt
-            quas illum! Tempora ratione voluptatibus consequuntur sed aliquam?
-            Culpa enim aliquid vero iusto vel totam in fugit quasi ab fugiat
-            quaerat tempora, veritatis aspernatur laudantium tempore molestiae
-            minima atque ad voluptates corrupti iure omnis doloremque explicabo.
-            Quis, odit ullam beatae unde, et autem sunt sequi eveniet molestias
-            ex itaque alias voluptatibus, vero quia eligendi ipsum minus.
-            Officia, iure!
-          </p>
-          <NavLink to={""} className={"project__btn"}>
-            Faça um orçamento
-          </NavLink>
-        </div>
-        <div className="project__container">
-          <h2 className="project__title">PROJETOS DE LARGA ESCALA</h2>
-          <div className="project__img__container">
-            <img className="project__img" src={projetosHome} alt="" />
-          </div>
-          <p className="project__text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            dicta animi voluptatibus voluptate. Delectus neque libero quis
-            laborum vitae deleniti deserunt facilis perspiciatis modi, earum,
-            praesentium tempora distinctio sunt accusamus recusandae totam nemo
-            eum natus sapiente! Asperiores ullam modi officia ducimus incidunt
-            quas illum! Tempora ratione voluptatibus consequuntur sed aliquam?
-            Culpa enim aliquid vero iusto vel totam in fugit quasi ab fugiat
-            quaerat tempora, veritatis aspernatur laudantium tempore molestiae
-            minima atque ad voluptates corrupti iure omnis doloremque explicabo.
-            Quis, odit ullam beatae unde, et autem sunt sequi eveniet molestias
-            ex itaque alias voluptatibus, vero quia eligendi ipsum minus.
-            Officia, iure!
+            A Big Control é uma empresa especializada em projetos e serviços de segurança eletrônica, rede, controle de acesso, segurança patrimonial, infraestrutura, entre outros.
+            Se segurança eletrônica fosse tudo igual não haveria tanta variedade de produtos e aplicativos.
+            Desenvolvemos projetos e soluções de acordo com a necessidade do seu condomínio ou empresa.
           </p>
           <NavLink to={""} className={"project__btn"}>
             Entre em contato
           </NavLink>
+
+        </div>
+        <div className="project__container">
+          <h2 className="project__title">PROJETOS COMPLETOS E EM LARGA ESCALA</h2>
+          <div className="project__img__container">
+            <img className="project__img" src={projetosHome} alt="" />
+          </div>
+          <div className="project__text">
+            <span>Infraestrutura de cabeamento</span>
+            <span>Infraestrutura de rede</span>
+            <span>Planta de Infraestrutura</span>
+            <span>Cabeamento estruturado</span>
+            <span>Tecnologia indicada</span>
+            <span>Segurança perimetral</span>
+            <span>Analíticos</span>
+            <span>Monitoramento em tempo real</span>
+            <span>Monitoramento remoto</span>
+            <span>Segurança dos equipamentos</span>
+            <span>Segurança das informações</span>
+            <span>Controle de acesso de pessoas e veículos</span>
+            <span>Mapa de rede</span>
+            <span>Manutenção</span>
+            <span>Locação</span>
+          </div>
+          <NavLink to={""} className={"project__btn"}>
+            Faça um orçamento
+          </NavLink>
+
         </div>
         <div className="project__container">
           <h2 className="project__title">CONSULTORIA DE SEGURANÇA</h2>
+          <span className="project__subtitle">Seu projeto pode sair de graça</span>
           <div className="project__img__container">
             <img className="project__img" src={projetosHome} alt="" />
           </div>
           <p className="project__text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            dicta animi voluptatibus voluptate. Delectus neque libero quis
-            laborum vitae deleniti deserunt facilis perspiciatis modi, earum,
-            praesentium tempora distinctio sunt accusamus recusandae totam nemo
-            eum natus sapiente! Asperiores ullam modi officia ducimus incidunt
-            quas illum! Tempora ratione voluptatibus consequuntur sed aliquam?
-            Culpa enim aliquid vero iusto vel totam in fugit quasi ab fugiat
-            quaerat tempora, veritatis aspernatur laudantium tempore molestiae
-            minima atque ad voluptates corrupti iure omnis doloremque explicabo.
-            Quis, odit ullam beatae unde, et autem sunt sequi eveniet molestias
-            ex itaque alias voluptatibus, vero quia eligendi ipsum minus.
-            Officia, iure!
+            Ao contratar o PROJETO DE SEGURANÇA da Big Control e posterior sua implantação, o valor do projeto é descontado de sua proposta de serviços e produtos.
           </p>
           <NavLink to={""} className={"project__btn"}>
             Agende conosco
