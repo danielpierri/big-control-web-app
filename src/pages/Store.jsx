581675afc06fe4728b37ca5f6d94e95507b16f44
Daniel Pierri <cyberpierri@gmail.com>
2023-07-07 16:13:20 -0300

    Added title to Store Route

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -22,7 +22,8 @@ export default function Store() {
   return (
     <div className="store">
       <Navbar />
-      <h1>Kits de Monitoramento - Locação e Manutenção - WEBSITE EM PRODUÇÃO</h1>
+      <h1 className="store__title">Kits de Monitoramento</h1>
+      <p className="store__subtitle">Consulte também nossos planos de <span>Locação</span> e <span>Manutenção</span></p>
       {product.length > 0 && (
         <div className="store__container">
           {product.map((product) => {
@@ -31,14 +32,14 @@ export default function Store() {
                 <h2 className="product__title">{product.title}</h2>
                 <img src={product.img} alt="anúncio de kit de câmeras" className="product__img" />
                 {/* <p className="product__caption">Kit acompanha</p>
-            <ul className="product__list">
-              <li>{product.recorder}</li>  
-              <li>{product.cameras}</li>  
-              <li>{product.storage}</li>  
-              <li>{product.cable}</li>  
-              <li>{product.powerSupply}</li>  
-              <li>{product.connectors}</li>  
-            </ul> */}
+                <ul className="product__list">
+                  <li>{product.recorder}</li>  
+                  <li>{product.cameras}</li>  
+                  <li>{product.storage}</li>  
+                  <li>{product.cable}</li>  
+                  <li>{product.powerSupply}</li>  
+                  <li>{product.connectors}</li>  
+                </ul> */}
                 <a href={`https://wa.me/5521984870140?&text=${encodeURI(product.wppMsg)}`} target="_blank" rel="noreferrer" className="product__link" >
                   Consulte valores
                 </a>
